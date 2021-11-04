05e7e452c41049baefa15701c8d8fa28fd278053
NellaDescodeuses <nellaruscade.pro@gmail.com>
2021-11-04 12:25:01 +0100

    maj

--- server.js
+++ server.js
@@ -2,14 +2,17 @@ const express = require("express");// le paquet http est maintenant disponible, 
 
 const app = express();
 
+app.use("/public", express.static(__dirname + "/public"));// sur le dossier public tu me renvoies TOUS les fichier contenu dans le dosssier public
+app.use("/dist", express.static(__dirname + "/dist"))// sur le dossier public tu me renvoies TOUS les fichier contenu dans le dosssier dist
+
 app.get('/', (req, res) => {
-    console.log("J'ai reçu une requête.");
 
-    res.send("Bonjour !");
+    res.sendFile(__dirname +"/index.html"); //__dirname permet de récupérer l'adresse du dossier dans lequel on se trouve.
 })
 
-app.get('/aurevoir', (req, res) => { // je crée un nouvelle page 
-    res.send("Au revoir !");
-})
+// app.get('/main.js', (req, res) => {
+
+//     res.sendFile(__dirname +"/dist/main.js"); //__dirname permet de récupérer l'adresse du dossier dans lequel on se trouve.
+// })
 
 app.listen(3000);
